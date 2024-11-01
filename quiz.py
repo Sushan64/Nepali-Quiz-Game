6c5116236102a6d5dd19b3c6505c4781d42baec0
Sushan Khatiwada <sushan.khatiwada2064@gmail.com>
2024-11-01 12:18:18 +0545

    Update main.py

--- quiz.py
+++ quiz.py
@@ -1,54 +1,24 @@
-questions = ("In which district is the ‘Fungfung’ waterfall?",
-             "How many heritage properties are listed in the World Heritage List?",
-             "Who was the first to use the word ‘Jai Nepal’?",
-             "How many national pride projects are there in Nepal right now?",
-             "What percentage of the total land area of Nepal is cultivated on land?",
-             "Which is Nepal’s first highway?",
-             "What is the name of road: Kathmandu – Pokhara?",
-             "What is called the blank sheet on the front and back of the book?",
-             "Who is the ‘Zero’ Inventor?",
-             "From which district did tea cultivation originate in Nepal?")
-
-
-options = (("A. Manaag", "B. Mustang", "C. Bhankuta", "D. Nuwakot"),
-          ("A. 9", "B. 10", "C. 11", "D. 12"),
-          ("A. Puspa Kamal Dahal", "B. BP Koirala", "C. Shukraraj Shastri", "D. Prithivi Narayana Shah"),
-          ("A. 21", "B. 22", "C. 23", "D. 24"),
-          ("A. 17.95%", "B. 17.96%", "C. 17.97%", "D. 17.87%"),
-          ("A. Araniko Highway", "B. Tribhuvan Highway", "C. Prithiwi Highway", "D. Mahendra Highway"),
-          ("A. BP Highway", "B. Prithwi Highway", "C. Araniko Highway", "D. Mahendra Highway"),
-          ("A. Fly Leaf", "B. No Paper", "C. Blank Cover", "D. Clean Leaf"),
-          ("A. Newton", "B. Balmeeki", "C. Arya Bhatt", "D. Albert Einstein"),
-          ("A. Jhapa", "B. Sholukhumbu", "C. Bhankuta", "D. ilam"))
-
-
-answer = ("D",
-         "B",
-         "C",
-         "C",
-         "C",
-         "B",
-         "B",
-         "A",
-         "C",
-         "D")
+#all file are self made which I provided
+from questions import questions as q
+from options import options as op
+from answer import answer as ans
 
 i = 0 #this is using in loop
 score = 0
 guess = []
-for question in questions:
+for question in q:
 
   print(question)
-  for o in range(0, len(options[i])):
-    if o == (len(options[i]) -1): #this is for not displaying comma in the last.
-      print(options[i][o])
+  for o in range(0, len(op[i])):
+    if o == (len(op[i]) -1): #this is for not displaying comma in the last.
+      print(op[i][o])
     else:
-      print(options[i][o], end=', ')
+      print(op[i][o], end=', ')
   user_choose = input("Enter (A, B, C, D): ").upper()
   
-  guess.append(user_choose) # add the user choice in the list guess[]
+  guess.append(user_choose) 
   
-  if user_choose == answer[i]:
+  if user_choose == ans[i]:
     print('—————————————')
     print('COORECT!')
     score += 1 #increasing the score of the user
@@ -61,16 +31,16 @@ for question in questions:
   i += 1
 
 print("Your guess: ", end="")
-for g in range(0, len(guess)): #again this is also same, not to display comma in the last e.g. if we don't use this code output will be like A,B,C,D, but after this output will be like A,B,C,D
+for g in range(0, len(guess)):
   if g == (len(guess) -1):
     print(guess[g])
   else:
     print(guess[g], end=", ")
 
 print("Correct Answers: ", end="")
-for a in range(0, len(answer)):
-  if a == (len(answer) -1):
+for a in range(0, len(ans)): #again this is also same, not to display comma in the last e.g. if we don't use this code output will be like A,B,C,D, but after this output will be like A,B,C,D
+  if a == (len(ans) -1):
     print(answer[a])
   else:
-    print(answer[a], end=", ")
+    print(ans[a], end=", ")
 print(f"Your score is {score}/{len(questions)}")
